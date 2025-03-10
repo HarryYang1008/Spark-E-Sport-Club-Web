263d875604892adda4d86df9c729f8e195ba04c0
Weikang Yang <2877929833@qq.com>
2025-03-10 14:31:12 -0400

    initial-2

--- server/index.js
+++ server/index.js
@@ -1,41 +1,19 @@
-// THESE ARE NODE APIs WE WISH TO USE
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-// CREATE OUR SERVER
-const PORT = 8080;
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-// SETUP THE MIDDLEWARE
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
+// Express setup
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// SETUP OUR OWN ROUTERS AS MIDDLEWARE
-const authStoreRouter = require('./auth_store_router')
-app.use('/auth_store', authStoreRouter)
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName')
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-// INITIALIZE OUR DATABASE OBJECT
-const mongoose = require('mongoose')
-const mongoDB = "mongodb://127.0.0.1:27017/customap";
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// PUT THE SERVER IN LISTENING MODE
-const server = app.listen(PORT, () => console.log('Server running on port '+PORT))
-process.on('SIGINT', () => {
-    server.close(() => {
-      console.log('\nServer closed')
-    })
-    if(db) {
-      db.close()
-        .then(() => console.log('Database instance disconnected'))
-        .catch((err) => console.log(err));
-    }
-  })
+// Server listening
+app.listen(5000, () => console.log('Server running on port 5000'));
